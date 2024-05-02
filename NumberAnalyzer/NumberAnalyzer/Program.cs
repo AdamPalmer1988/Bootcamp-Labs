6142bed9eb796e0c5ff56f57eea204e66b88938a
Adam Palmer <adampalmer@rocketmortgage.com>
2024-05-01 22:27:04 -0400

    Number Analyzer and Room Calculator Lab

---
+++ NumberAnalyzer/NumberAnalyzer/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine("Welcome to the Number Analyzer");
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+
+Console.WriteLine("Hello " + name);
+
+string exit = "yes";
+
+while (exit == "yes")
+{
+    Console.WriteLine(name + "Please enter a number 1-100");
+    string stringNumber = Console.ReadLine();
+
+    int number = int.Parse(stringNumber);
+    while (number > 0 && number <= 100)
+    {
+        if ((number < 60) && (number % 2 == 1))
+        {
+            Console.WriteLine(name + ", your number is odd and less than 60");
+            number = 101;
+        }
+        else if ((number < 25 && number >= 2) && (number % 2 == 0))
+        {
+            Console.WriteLine(name + ", your number is even and less than 25");
+            number = 101;
+        }
+        else if ((number > 25 && number <= 60) && (number % 2 == 0))
+        {
+            Console.WriteLine(name + ", even and between 26 and 60 inclusive");
+            number = 101;
+        }
+        else if ((number % 2 == 0) && (number > 60))
+        {
+            Console.WriteLine(name + ", even and greater than 60");
+            number = 101;
+        }
+        else if ((number % 2 == 1) && (number > 60))
+        {
+            Console.WriteLine(name + ", odd and greater than 60");
+            number = 101;
+        }
+    }
+
+    if (number < 0)
+    {
+        Console.WriteLine(name + ", your number is not a positive integer ");
+    }
+
+    Console.WriteLine(name + ", would you like to continue? yes or no?");
+        exit = Console.ReadLine();
+
+}
---
+++ RoomCalculatorLab/RoomCalculatorLab/Program.cs
@@ -0,0 +1,35 @@
+﻿//enter values of length and width
+Console.WriteLine("Welcome to the Skynet Room Detail Generator!");
+
+Console.WriteLine("Please enter the Length of your Room:");
+string stringLength = Console.ReadLine();
+
+int Length = int.Parse(stringLength);
+
+Console.WriteLine("Please enter the Width of your Room:");
+string stringWidth = Console.ReadLine();
+
+int Width = int.Parse(stringWidth);
+
+//calculations of Area and Perimeter 
+
+double area = (Length * Width);
+double perimeter = ((2 * Length) + (2 * Width));
+
+Console.WriteLine("The Area of your room is " + area);
+Console.WriteLine("The Perimeter of your room is " + perimeter);
+
+if (area <= 250)
+{
+    Console.WriteLine("Your room is smaller room!");
+}
+ else if (area >= 650 )
+{
+    Console.WriteLine("Your room is larger in size!");
+}
+else
+{
+    Console.WriteLine("Your room is medium-sized room!");
+}
+
+Console.WriteLine("Thank you for using the Room Detail Generator!");
