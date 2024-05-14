3b8d8782c2d8c23d5c2c90d888325d3e41048453
Adam Palmer <adampalmer@rocketmortgage.com>
2024-05-13 21:25:10 -0400

    Basic For Loop

---
+++ basicForLoop/basicForLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿int runningTotal = 0;
+string playAgain = "Yes";
+string responseString;
+int response;
+
+while (playAgain == "Yes")
+{
+    Console.WriteLine("Enter a number:");
+    responseString = Console.ReadLine();
+    response = int.Parse(responseString);
+
+    for (int i = 0; i <= response; i++)
+    {
+        runningTotal += i;
+    }
+
+    Console.WriteLine($"Your total is {runningTotal}!");
+    Console.WriteLine("Would you like to play again? Yes or No");
+    playAgain = Console.ReadLine();
+    runningTotal = 0;
+
+}
