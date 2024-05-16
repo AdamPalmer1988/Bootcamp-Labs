13e94cc5f0c5104e05bc214a5dfb4876f4d3a8a8
Adam Palmer <adampalmer@rocketmortgage.com>
2024-05-16 19:57:51 -0400

    Assessment #1

---
+++ Assessment1/Assessment1/Program.cs
@@ -0,0 +1,45 @@
+﻿static bool IsPassing(double grade)
+{
+    if (grade > 65)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+static double AverageGrades(double grade1, double grade2, double grade3)
+{
+    double average = (grade1 + grade2 + grade3) / 3;
+    return average;
+}
+
+static string OddOrEvenAndPassing(double grade)
+{
+    int intGrade = (int)grade;
+
+    if (IsPassing(grade) && (intGrade % 2 == 1))
+    {
+        return "passing and odd";
+    }
+    else if ((intGrade % 2 == 1))
+    {
+        return "failing and odd";
+    }
+    else if (IsPassing(grade) && (intGrade % 2 == 0))
+    {
+        return "passing and even";
+    }
+    else if ((intGrade % 2 == 0))
+    {
+        return "failing and even";
+
+    }
+    else
+    {
+        return "";
+    }
+}
+
