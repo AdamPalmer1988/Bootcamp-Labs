35e925ac88f3dbe1d3ebfd6556b76311a26648c1
Adam Palmer <adampalmer@rocketmortgage.com>
2024-05-20 21:37:44 -0400

    Basic String Processing Lab

---
+++ basicStringProcessing/basicStringProcessing/Program.cs
@@ -0,0 +1,34 @@
+﻿//First part of Lab
+using System;
+using System.Threading.Tasks.Dataflow;
+
+Console.WriteLine("Enter a sentence: ");
+
+string userInput = Console.ReadLine();
+
+string[] userInputWithWords = userInput.Split(' ');
+
+for (int i = 0; i < userInputWithWords.Length; i++)
+{
+    Console.WriteLine($"{userInputWithWords[i]}");
+}
+
+//Second part of Lab
+
+List<string> namesList  = new List<string>();
+
+string keepPlaying = "yes";
+
+while (keepPlaying == "yes")
+{
+    Console.WriteLine("Enter some text: ");
+    string someText = Console.ReadLine();
+
+    namesList.Add(someText);
+    string joinedText = string.Join(" ", namesList);
+
+    Console.WriteLine($"You have entered: {joinedText} ");
+    Console.WriteLine("Would you like to continue? yes or no?");
+    keepPlaying = Console.ReadLine();
+
+}
