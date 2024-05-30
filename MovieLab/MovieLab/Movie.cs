7d0dfab5af87a12d04917fee383d8c15cc8372d6
Adam Palmer <adampalmer@rocketmortgage.com>
2024-05-29 21:53:04 -0400

    Movie Database Lab

---
+++ MovieLab/MovieLab/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLab;
+
+public class Movie
+{
+    //properties
+
+    public string Title { get; set; }
+    public string Catagory { get; set; }
+
+    //constructors
+    //the class and the constructor will have the same name
+
+    public Movie(string Title, string Catagory)
+    {
+        this.Title = Title;
+        this.Catagory = Catagory;
+    }
+
+    //Methods
+
+
+}
+
+
---
+++ MovieLab/MovieLab/Program.cs
@@ -0,0 +1,60 @@
+﻿using MovieLab;
+using System.ComponentModel.Design;
+
+//First Way
+//List<Movie> movieList = new List<Movie>();
+
+//instantiation
+// Movie movie1 = new Movie("Fast and the Furious 12", "Action Movie");
+
+// movieList.Add(movie1);
+
+List<Movie> movieList = new List<Movie>()
+{
+    new Movie("Toy Story", "Children"),
+    new Movie("Interstellar", "Sci-Fi"),
+    new Movie ("Scary Movie", "Comedy"),
+    new Movie("Kingpin", "Comedy"),
+    new Movie("Titanic", "Drama"),
+    new Movie("Die Hard", "Action"),
+    new Movie("Frozen", "Children" ),
+    new Movie("Ted", "Comedy"),
+    new Movie("The Exoricist", "Horror"),
+    new Movie("Avatar", "Action")
+};
+
+Console.WriteLine("Welcome to the Movie List Application!");
+Console.WriteLine("There are " + movieList.Count + " movies in this list");
+Console.WriteLine("What catagory are you interested in?");
+
+string inputCatagory = Console.ReadLine();
+
+void CatagorySearch(List<Movie> movieList, string userCatagory)
+{
+    for (int i = 0; i < movieList.Count(); i++)
+    {
+        if (movieList[i].Catagory == userCatagory)
+        {
+            Console.WriteLine(movieList[i].Title);
+        }
+    }
+}
+
+string keepPlaying = "yes";
+
+while (keepPlaying == "yes")
+{
+    CatagorySearch(movieList, inputCatagory);
+
+    Console.WriteLine("Would you like to keep going? yes or no?");
+    keepPlaying = Console.ReadLine();
+
+    if (keepPlaying == "yes")
+    {
+        Console.WriteLine("What catagory are you interested in?");
+        inputCatagory = Console.ReadLine();
+    }
+    else
+        keepPlaying = "no";
+    
+}
