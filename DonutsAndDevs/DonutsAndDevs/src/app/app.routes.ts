6fb84b91713a0a6ffda933d10f4ad56702c5fe50
Adam Palmer <adampalmer@rocketmortgage.com>
2024-09-16 12:37:53 -0400

    Need to figure out how to add the famous people to the home page.

--- DonutsAndDevs/DonutsAndDevs/src/app/app.routes.ts
+++ DonutsAndDevs/DonutsAndDevs/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DonutsComponent } from './donuts/donuts.component';
+import { DevsComponent } from './famous-people/famous-people.component';
 
 export const routes: Routes = [
-    {path: '', component:DonutsComponent}
+    {path: '', component:DonutsComponent},
+    {path: '', component:DevsComponent}
 ];
